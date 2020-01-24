53cbe9f3ad1ada6cde18f514049c5342f6f7d8b6
RuCybernetic <RuCybernetic@yandex.ru>
2020-01-25 01:37:28 +0300

    Update Join.py

--- cogs/events/Join.py
+++ cogs/events/Join.py
@@ -1,6 +1,5 @@
 import discord
 import random
-import os
 from discord.ext import commands
 
 class OnMemberJoinCog(commands.Cog):
@@ -9,13 +8,13 @@ class OnMemberJoinCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        guild_id = os.environ.get('guild_id')
-        bot_channel_id = os.environ.get('bot_channel_id')
-        if member.guild.id == os.environ.get(int(guild_id)):
+        guild_id = int(os.environ.get('guild_id'))
+        bot_channel_id = int(os.environ.get('bot_channel_id'))
+        if member.guild.id == guild_id:
             role = discord.utils.get(member.guild.roles, name='Гость')
             await member.add_roles(role)
             a = ('зови своих друзей', 'пиши если чо', 'заходи не стесняйся')
-            channel = self.bot.get_channel(int(bot_channel_id))
+            channel = self.bot.get_channel(bot_channel_id)
             em = discord.Embed(description=f'{member.mention}, {random.choice(a)}', color=0x00b820)
             em.set_author(name='Добро пожаловать на сервер')
             await channel.send(embed=em)
