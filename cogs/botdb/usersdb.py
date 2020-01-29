433fac9629500b53901a6baf57b21fd6cf624a99
RuCybernetic <RuCybernetic@yandex.ru>
2020-01-29 21:53:54 +0300

    Update usersdb.py

--- cogs/botdb/usersdb.py
+++ cogs/botdb/usersdb.py
@@ -18,7 +18,7 @@ class OnMessageCog(commands.Cog):
             for member in guild.members:
                 cursor.execute(f"SELECT id FROM users WHERE id={member.id}")
                 if cursor.fetchone() is None:
-                    cursor.execute(f"INSERT INTO users VALUES ({member.id}, '{member.name}', '<@{member.id}>', 0, 0)")
+                    cursor.execute(f"INSERT INTO users VALUES ({member.id}, '{member.name}', '<@{member.id}>', 0, 0, 0, 0)")
                 else:
                     pass
                 conn.commit()
@@ -29,15 +29,49 @@ class OnMessageCog(commands.Cog):
         cursor = conn.cursor()
         cursor.execute(f"SELECT id FROM users WHERE id={member.id}")
         if cursor.fetchone() is None:
-            cursor.execute(f"INSERT INTO users VALUES ({member.id}, '{member.name}', '<@{member.id}>', 0, 0)")
+            cursor.execute(f"INSERT INTO users VALUES ({member.id}, '{member.name}', '<@{member.id}>', 0, 0, 0, 0)")
         else:
             pass
         conn.commit()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-
-        pass
+        logs = self.bot.get_channel(669129314251309056)
+        try:
+            await self.bot.process_commands(message)
+        except Exception:
+            pass
+        if message.author == self.bot.user:
+            pass
+        else:
+            conn = sqlite3.connect("database.db")
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT * FROM users WHERE id={message.author.id}")
+            res = cursor.fetchall()
+            if not res:
+                cursor.execute(f"INSERT INTO users (id, xp, lvl, msg) VALUES ({message.author.id}, 50, 1, 1)")
+                conn.commit()
+                conn.close()
+            else:
+                res = res[0]
+                xp = res[3]
+                lvl = res[4]
+                msg = res[5]
+                print(msg)
+                msg += 1
+                xp += 1000
+                if xp >= 1000 + 100 * lvl:
+                    xp = xp - (1000 + 100 * lvl)
+                    lvl += 1
+                    if message.author == self.bot.user:
+                        pass
+                    else:
+                        await logs.send(f"Поздравляю! {message.author} Вы получили level! Теперь ваш уровень **{lvl}**")
+                else:
+                    pass
+                cursor.execute(f"UPDATE users SET lvl={lvl}, xp={xp}, msg={msg} WHERE id={message.author.id}")
+                conn.commit()
+                conn.close()
 
     @commands.command()
     async def my_info(self, message):
